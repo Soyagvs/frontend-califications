aeab815ca8013ab8976d210ad74191d8135502ad
Agustin Martinez <agustinmartinezyt@gmail.com>
2025-10-25 13:36:05 -0300

    refactor: clean up Card component code and improve button rendering logic

--- src/card/card.tsx
+++ src/card/card.tsx
@@ -23,7 +23,6 @@ const numberArray = [
 
 export const Card = () => {
     const [select, setSelect] = useState<number | null>(null)
-
     const [submit, setSubmit] = useState<boolean>(false)
 
     const handleSelect = (index: number) => {
@@ -32,15 +31,14 @@ export const Card = () => {
 
     const handleSubmit = () => {
         setSubmit(true)
-        console.log("valor true", select)
     }
 
-    if(submit){
-        return <CardSubmit valor={select}/>
+    if (submit) {
+        return <CardSubmit valor={select} />
     }
 
     return (
-        <div className="bg-[#1F2630] w-[30em]  rounded-4xl p-9">
+        <div className="bg-[#1F2630] sm:w-[30em] w-[90%] rounded-4xl sm:p-9 p-5">
             <div className="size-15 bg-[#252F39] rounded-full flex items-center justify-center">
                 <StarIcon />
             </div>
@@ -48,21 +46,14 @@ export const Card = () => {
                 <h2 className="text-3xl font-bold text-white -mb-5">How did we do?</h2>
                 <p className="text-neutral-500">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
 
-                <div className="flex gap-5 justify-between items-center text-neutral-400 text-lg font-semibold">
+                <div className="flex flex-wrap  sm:gap-5 sm:justify-between items-center text-neutral-400 text-lg font-semibold">
                     {numberArray.map(({ number }, index) => {
                         const isActive = select === index
                         return (
                             <div key={index}>
-                                {isActive?
-                                    <button
-                                        onClick={() => handleSelect(index)}
-                                        className="size-15 bg-[#FC7613] rounded-full flex items-center justify-center hover:bg-neutral-500 cursor-pointer">{number}</button>
-                                        : <button
-                                        
-                                        onClick={() => handleSelect(index)}
-                                        className="size-15 bg-[#252F39] rounded-full flex items-center justify-center hover:bg-neutral-500 cursor-pointer">{number}</button>
-
-                                }
+                                <button
+                                    onClick={() => handleSelect(index)}
+                                    className={`size-15 rounded-full flex items-center justify-center hover:bg-neutral-500 cursor-pointer ${isActive ? 'bg-[#FC7613]' : 'bg-[#252F39]'}`}>{number}</button>
                             </div>
 
                         )

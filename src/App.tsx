d569af18624ca9e06e004ea05eb5dfc8b2747129
Agustin Martinez <agustinmartinezyt@gmail.com>
2025-10-25 01:07:24 -0300

    feat: initialize application structure with App, main, and index files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Card } from "./card/card"
+
+function App() {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center mx-auto bg-[#141519]">
+      <Card/>
+    </div>
+  )
+}
+
+export default App
